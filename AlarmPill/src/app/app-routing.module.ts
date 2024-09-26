1a18ad920658871a5e3cbfb29660f4e803dac72c
Jeniffer Corredor <j.corredore@uniandes.edu.co>
2024-09-26 17:38:48 -0500

    Adición enrutamiento

--- AlarmPill/src/app/app-routing.module.ts
+++ AlarmPill/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './components/main/main.component';
+import { MedicinesComponent } from './components/medicines/medicines.component';
+import { AdministrationComponent } from './components/administration/administration.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'medicines', component: MedicinesComponent },
+  { path: 'administration', component: AdministrationComponent },
 ];
 
 @NgModule({
--- AlarmPill/src/app/app.module.ts
+++ AlarmPill/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MainComponent } from './components/main/main.component';
+import { MedicinesComponent } from './components/medicines/medicines.component';
+import { AdministrationComponent } from './components/administration/administration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MainComponent,
+    MedicinesComponent,
+    AdministrationComponent
   ],
   imports: [
     BrowserModule,
